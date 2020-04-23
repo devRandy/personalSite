ed93a7632332cff837a3fb5b386855ef1c4c49e7
Randy Jimenez <randyjimenez2010@gmail.com>
2020-04-23 19:13:08 -0400

    add screen orientation lock

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 function init() {
+    screen.orientation.lock("portrait");
+
     Window.mymap = new L.map('mapid').setView([35.227772, -80.840916], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
